9586bc530430d1b373af26bca925e29f9a263373
Kevin J. Lynagh <kevin@keminglabs.com>
2012-07-09 08:48:15 -0700

    Switch histograms from overlaid, multiple-colors to stacked facets.

--- src/cljs/vcfvis/histogram.cljs
+++ src/cljs/vcfvis/histogram.cljs
@@ -10,45 +10,55 @@
             [c2.svg :as svg]
             [c2.ticks :as ticks]))
 
-(def margin 40)
+(def margin 20)
 
 ;;width and height of data frame
-(def height 400)
+(def height 200)
 (def width 900)
 
-(bind! "#histogram"
-       [:svg#histogram {:height (+ height (* 2 margin))
-                        :width  (+ width (* 2 margin))}
-        
-        (let [vcfs @core/!vcfs]
-          (when (seq vcfs)
-            (let [metric-extent (get-in (core/vcf-metric (first vcfs)
-                                                  (@core/!metric :id))
-                                 [:x-scale :domain])
-                  {:keys [extent ticks]} (ticks/search metric-extent
-                                                       :length width)
-                  x (scale/linear :domain extent
-                                  :range [0 width])]
-              [:g {:transform (svg/translate [margin margin])}
-               [:g.data-frame {:transform (str (svg/translate [0 height])
-                                               (svg/scale [1 -1]))}
-                (let [metric-id (@core/!metric :id)
-                      metrics (map #(core/vcf-metric % metric-id) vcfs)
-                      max-val (apply max (flatten (map :vals metrics)))
-                      y (scale/linear :domain [0 max-val]
-                                      :range [0 height])
-                      bin-width (:bin-width (core/vcf-metric (first vcfs) metric-id))
-                      dx (- (x bin-width) (x 0))]
+(defn histograms* [vcfs x-scale]
+  (let [metric-id (@core/!metric :id)
+        metrics (map #(core/vcf-metric % metric-id) vcfs)
+        max-val (apply max (flatten (map :vals metrics)))
+        y (scale/linear :domain [0 max-val]
+                        :range [0 height])
+        ;;The bin width is the same for a given metric across all samples
+        bin-width (:bin-width (core/vcf-metric (first vcfs) metric-id))
+        dx (- (x-scale bin-width) (x-scale 0))]
 
-                  (unify metrics
-                         (fn [metric]
-                           [:g.distribution
-                            (map-indexed
-                             (fn [idx v]
-                               [:rect {:x (* dx idx) :width dx
-                                       :height (y v)}])
-                             (metric :vals))])))]
-               [:g.axis.ordinate]
-               [:g.axis.abscissa {:transform (svg/translate [0 height])}
-                (svg/axis x ticks :orientation :bottom)
-                ]])))])
+    [:div.span12
+     (unify (map vector vcfs (repeat metric-id))
+            (fn [[vcf metric-id]]
+              [:div.histogram
+               [:span.label (vcf :filename)]
+               [:svg {:width (+ width (* 2 margin)) :height (+ height (* 1.1 margin))}
+                [:g {:transform (svg/translate [margin margin])}
+                 [:g.distribution {:transform (str (svg/translate [0 height])
+                                                   (svg/scale [1 -1]))}
+                  (map-indexed (fn [idx v]
+                                 [:rect {:x (* dx idx) :width dx
+                                         :height (y v)}])
+                               ((core/vcf-metric vcf metric-id) :vals))]]]]))]))
+
+(bind! "#histograms"
+       (let [vcfs @core/!vcfs]
+         (when (seq vcfs)
+           (let [metric-extent (get-in (core/vcf-metric (first vcfs)
+                                                        (@core/!metric :id))
+                                       [:x-scale :domain])
+                 {:keys [extent ticks]} (ticks/search metric-extent
+                                                      :length width)
+                 x (scale/linear :domain extent
+                                 :range [0 width])]
+
+             [:div.row#histograms
+
+              ;;histogram distributions
+              (histograms* vcfs x)
+
+              ;;x-axis
+              [:div.span12
+               [:div.axis.abscissa
+                [:svg {:width (+ width (* 2 margin)) :height 20}
+                 [:g {:transform (svg/translate [margin 2])}
+                  (svg/axis x ticks :orientation :bottom)]]]]]))))
