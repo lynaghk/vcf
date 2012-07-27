a700b172954c822ebd528b22ea4b756cc7c8e034
Kevin J. Lynagh <kevin@keminglabs.com>
2012-07-27 11:44:45 -0700

    Update bcbio submodule and force Jetty to use stderr logger so it doesn't pick up the wrong version of sl4j in the classpath.

--- project.clj
+++ project.clj
@@ -2,34 +2,34 @@
   :description "Genetic variant metric visualization tool"
   :license {:name "BSD" :url "http://www.opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 
+
                  [com.keminglabs/c2 "0.2.1-SNAPSHOT"]
                  [com.keminglabs/chosen "0.1.7-SNAPSHOT"]
-                 
+
                  [compojure "1.1.1"]
                  [ring/ring-core "1.1.1"]
                  [ring/ring-jetty-adapter "1.1.1"]
                  [com.cemerick/friend "0.0.9"]
                  
-                 [bcbio.variation "0.0.1-SNAPSHOT"
-                  :exclusions [org.clojars.chapmanb/gatk-lite]
-                  ]]
+                 [bcbio.variation "0.0.1-SNAPSHOT"]]
+  
+  :jvm-opts ["-Dorg.eclipse.jetty.util.log.class=org.eclipse.jetty.util.log.StdErrLog"]
   
   :profiles {:dev {:dependencies [[midje "1.4.0"]
                                   [clj-http "0.5.0"]]}}
   
   :min-lein-version "2.0.0"
   :source-paths ["src/clj" "src/cljs"]
-  
+
   :plugins [[lein-cljsbuild "0.2.4"]]
 
   :cljsbuild {:builds
               [{:source-path "src/cljs"
                 :compiler {:output-to "public/vcf.js"
-                           
+
                            ;; :optimizations :advanced
                            ;; :pretty-print false
-                           
+
                            :optimizations :whitespace
                            :pretty-print true
 
