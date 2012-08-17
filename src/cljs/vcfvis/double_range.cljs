51c1d09a36a8bc0460b8d53c5eaa1259a5139ed1
Kevin J. Lynagh <kevin@keminglabs.com>
2012-08-17 08:21:31 -0700

    Double range selector should only fire callback if value has actually changed.

--- src/cljs/vcfvis/double_range.cljs
+++ src/cljs/vcfvis/double_range.cljs
@@ -3,14 +3,17 @@
   (:use-macros [c2.util :only [pp p bind!]])
   (:require [c2.dom :as dom]
             [goog.events :as gevents]
-            [goog.fx.Dragger :as goog.fx.Dragger]
             [goog.ui.TwoThumbSlider :as goog.ui.TwoThumbSlider]))
 
 (defn init! [el callback]
   (let [$tt (dom/->dom el)
-        tt (new goog.ui.TwoThumbSlider)]
+        tt (new goog.ui.TwoThumbSlider)
+        !old-val (atom [])]
     (doto tt
       (.decorate $tt)
       (gevents/listen goog.ui.Component.EventType.CHANGE
-                      #(let [v (.getValue tt)]
-                         (callback [v (+ v (.getExtent tt))]))))))
+                      #(let [v (.getValue tt)
+                             extent [v (+ v (.getExtent tt))]]
+                         (when (not= @!old-val extent)
+                           (reset! !old-val extent)
+                           (callback extent)))))))
