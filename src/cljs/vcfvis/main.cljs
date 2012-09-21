800fa539c2ecc8365de24b2023dc8f4827e265ac
chapmanb <chapmanb@50mail.com>
2012-09-21 17:01:35 -0400

    Basic support for modal dialogs during data load and processing. Needs work to handle multiple file loading correctly

--- src/cljs/vcfvis/main.cljs
+++ src/cljs/vcfvis/main.cljs
@@ -2,6 +2,7 @@
   (:use-macros [c2.util :only [pp p timeout]]
                [reflex.macros :only [constrain!]]
                [dubstep.macros :only [publish!]])
+  (:use [c2.util :only [clj->js]])
   (:require [vcfvis.core :as core]
             [vcfvis.data :as data]
             ;;[vcfvis.stub :as data]
@@ -15,4 +16,6 @@
                  (data/load-vcf f (fn [vcf] (publish! {:vcf vcf})))))))
 
 ;;Request file list from server
+(.modal (js/jQuery "#waiting-modal")
+        (clj->js {:backdrop "static" :keyboard false :show true}))
 (data/load-context (partial reset! core/!context))
